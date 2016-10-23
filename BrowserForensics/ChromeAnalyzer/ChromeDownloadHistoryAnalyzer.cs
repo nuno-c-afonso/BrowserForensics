2b92660735e7ab0c68ea1c7ecbaac28152c594db
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-10-23 10:35:48 +0100

    Added class to handle the DB

--- BrowserForensics/ChromeAnalyzer/ChromeDownloadHistoryAnalyzer.cs
+++ BrowserForensics/ChromeAnalyzer/ChromeDownloadHistoryAnalyzer.cs
@@ -8,27 +8,20 @@ using System.Data;
 
 namespace ChromeAnalyzer {
     public class ChromeDownloadHistoryAnalyzer : BrowserAnalyzer.DownloadHistoryAnalyzer {
-        private SQLiteConnection dbConnection;
+        private SQLite.Client client;
 
         public ChromeDownloadHistoryAnalyzer(string location) {
-            Console.WriteLine("\n\n" + location + "\n\n");
-            Console.ReadLine();
-
-            dbConnection = new SQLiteConnection("Data Source=" + location);
+            client = new SQLite.Client(location);
         }
 
         public string getDownloads() {
-            DataTable completedDownloads = new DataTable();
-            string s = "SELECT target_path AS path, tab_url AS url FROM downloads WHERE received_bytes = total_bytes AND total_bytes > 0;";
+            DataTable completedDownloads;
+            string s = "SELECT target_path AS path, tab_url AS url " +
+                       "FROM downloads " + 
+                       "WHERE received_bytes = total_bytes AND total_bytes > 0;";
 
-            dbConnection.Open();
-            SQLiteCommand query = new SQLiteCommand(dbConnection);
-            query.CommandText = s;
-            SQLiteDataReader reader = query.ExecuteReader();
-            completedDownloads.Load(reader);
-            reader.Close();
-            dbConnection.Close();
-
+            completedDownloads = client.select(s);
+            
             s = "";
             foreach(DataRow r in completedDownloads.Rows) {
                 s += r["path"] + " <- " + r["url"] + "\r\n";
---
+++ BrowserForensics/SQLite/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite {
+    public class Client {
+        private SQLiteConnection dbConnection;
+
+        public Client(string location) {
+            dbConnection = new SQLiteConnection("Data Source=" + location);
+        }
+
+        public DataTable select(string query) {
+            DataTable table = new DataTable();
+
+            dbConnection.Open();
+            SQLiteCommand command = new SQLiteCommand(dbConnection);
+            command.CommandText = query;
+            SQLiteDataReader reader = command.ExecuteReader();
+            table.Load(reader);
+            reader.Close();
+            dbConnection.Close();
+
+            return table;
+        }
+    }
+}
